4e36e762870a515fbe4ca37c49a684e3c15ec52d
Aryan Jaya <aryanjaya88@gmail.com>
2022-06-19 11:34:39 +0800

    display add contact data

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,29 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import './Contact.css';
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <section className="content">
+      <div className="left">
+        <img
+          src={data.photo}
+          alt={data.name}
+          className="rounded"
+        />
+      </div>
+      <div className="right">
+        <div className="container">
+          <section>
+            <p className="header">{data.name}</p>
+            <p className="sub-header">{data.phone}</p>
+            <p className="sub-content">{data.email}</p>
+          </section>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <header>
+      <h1 className="title">Call a Friend</h1>
+      <p className="sub-title">your friendly contact app</p>
+    </header>
+  );
+};
 
 export default Header;
